658ebbdfd7d8d7ab6f8fb1d973a5739358562a6e
Muhammad Ali Khalid <muhammadalikhalid101@gmail.com>
2024-06-01 11:59:25 +0500

    update task3.cpp

---
+++ task4.cpp
@@ -0,0 +1,49 @@
+/*
+
+Write a C++ program that works with dynamic 1-dimensional arrays:
+● Create a dynamic array of integers of size n.
+● Initialize the array with values.
+● Print the elements of the array.
+● Implement functions to delete an element from the array and add an element.
+
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void addElement(vector<int> &temp) {
+    int element;
+    cout << "Enter element to add: ";
+    cin >> element;
+    temp.push_back(element);
+}
+
+void removeElement(vector<int> &temp, int &index) {
+    temp.erase(temp.begin() + index);
+}
+
+void display(vector<int> &temp) {
+    for (int &i: temp) {
+        cout << i << "    ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int size = 0, index;
+    cout << "Enter the size of array: ";
+    cin >> size;
+    vector<int> arr(size);
+    cout << "Enter values" << endl;
+    for (int &i: arr) {
+        cin >> i;
+    }
+    addElement(arr);
+    display(arr);
+    cout << "Enter index to remove an element: ";
+    cin >> index;
+    removeElement(arr, index);
+    display(arr);
+    return 0;
+}
