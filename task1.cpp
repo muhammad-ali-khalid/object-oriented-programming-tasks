84aeeb56fb46dd8c0c4cb067d6c82f2389c31365
Muhammad Ali Khalid <muhammadalikhalid101@gmail.com>
2024-06-01 10:38:22 +0500

    update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,8 +1,21 @@
+/*
+
+Write a C++ program that demonstrates dynamic memory allocation:
+● Use the new operator to allocate memory for an integer variable.
+● Initialize the allocated memory with a value.
+● Print the value and address of the dynamically allocated memory.
+● Use the delete operator to free the allocated memory.
+
+*/
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, world!";
+    int *ptr = new int;
+    *ptr = 10;
+    cout << "Value: " << *ptr << endl;
+    cout << "Address: " << ptr << endl;
+    delete ptr;
     return 0;
 }
