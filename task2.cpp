9437952efe6bfe41a813e051761b322a11a34d12
Muhammad Ali Khalid <muhammadalikhalid101@gmail.com>
2024-06-01 10:47:50 +0500

    update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,3 +1,20 @@
-//
-// Created by muhammad-ali on 6/1/24.
-//
+/*
+
+Write a C++ program that performs the following operations using pointers:
+● Declare two integer variables and initialize them with values.
+● Declare two pointer variables and point them to the respective integer variables.
+● Perform addition and subtraction operations using pointers and print the results.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10, b = 15;
+    int *ptr1 = &a;
+    int *ptr2 = &b;
+    cout << "Addition: " << *ptr1 + *ptr2 << endl;
+    cout << "Subtraction: " << *ptr1 - *ptr2 << endl;
+    return 0;
+}
